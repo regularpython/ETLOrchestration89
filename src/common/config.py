fe2da456ba3c8135373966a915385e8f55cc635f
Sairam <dsairam1992@gmail.com>
2025-07-22 09:19:26 +0530

    Code input added

---
+++ src/common/config.py
@@ -0,0 +1,7 @@
+
+rds_access_protocol = 'mysql+mysqlconnector'
+rds_username = 'admin'
+rds_password = 'Sairam123456789'
+rds_host = 'database-1.cb6myg0y0nlx.us-east-1.rds.amazonaws.com'
+rds_port = '3306'
+rds_db_name = 'batch89'
